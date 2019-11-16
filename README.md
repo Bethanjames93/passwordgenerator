90ab3b1857690699b5f23c5b44d63a5305da2a80
Bethan James <bethanjames@Bethans-MacBook-Pro.local>
2019-11-16 22:43:32 +0000

    adding the start of the javascript

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # passwordgenerator
+    The aim- to crete a password generator that will prompt the user to choose a password length between 8-128 then alert the user to choose if they would like to use numbers, uppercase, lowercase or special characters before generating the user with a password.
+    Bonus- give the user the option to save the password to their clipboard.
+
+## HTML
+    * Set up an index.hmtl including a heading "Password Generator"
+    * Create a content box which includes a text box that will reveal the password.
+    * Add two buttons: 'Generate Password' and 'Copy to Clipboard'
+
+## Javascript
+    * Set varibles for:
+        *Uppercase Characters
+        *Lowercase Characters
+        *Special Characters
+        *Numbers
+
+    * Create a prompt- "How many characters would you like your password to contain?"
+        * Create an alert if the password is under 8 characters or higher than 129
+
+    • Create a confirm asking the user to click ok to confirm adding a number, special character, lowercase character and uppercase character.
--- script.js
+++ script.js
@@ -2,3 +2,37 @@ var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specialCharacters = ["~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "}", "[", "]", "?", "/", "<", ">"];
+
+function passwordSelection () {
+    var length = prompt("How many characters would you like your password to contain?");
+
+    if (length < 8) {
+        alert("Password length must be at least 8 characters");
+        return;
+    }
+
+    if (length > 128) {
+        alert("Password length much be less than 129 characters");
+        return;
+    }
+
+var addSpecialCharacters = confirm("Click ok to confirm adding special characters");
+var addNumbers = confirm("Click ok to confirm using numbers");
+var addLowerCase = confirm("Click ok to confirm using lowercase characters");
+var addUpperCase = confirm("Click ok to confirm using uppercase characters");
+
+var passwordSelection = {
+    length,
+    addSpecialCharacters,
+    addNumbers,
+    addLowerCase,
+    addUpperCase,
+};
+
+return passwordSelection;
+
+}
+
+function generatePassword () {
+ 
+}
